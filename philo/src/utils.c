afbd804cf6112352aee37f955897f47d27e7af8f
Mai Chi Tran Nhat <maichibod@gmail.com>
2025-10-19 19:01:11 +0200

    cleanup function

--- philo/src/utils.c
+++ philo/src/utils.c
@@ -6,7 +6,7 @@
 /*   By: mtran-nh <mtran-nh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 15:15:03 by mtran-nh          #+#    #+#             */
-/*   Updated: 2025/10/10 16:31:44 by mtran-nh         ###   ########.fr       */
+/*   Updated: 2025/10/19 18:41:36 by mtran-nh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,19 @@ void	ft_usleep(size_t mls)
 	size_t	start;
 
 	start = get_current_time();
-	while ((get_current_time() - start) > mls)
+	while ((get_current_time() - start) < mls)
 		usleep(500);
+}
+
+void	cleanup(t_philos *philos, pthread_mutex_t *forks,
+		pthread_mutex_t *print_mutex)
+{
+	int i;
+	int n;
+
+	n = philos[0].philos_count;
+	i = -1;
+	while (++i < n)
+		pthread_mutex_destroy(&forks[i]);
+	pthread_mutex_destroy(print_mutex);
 }
