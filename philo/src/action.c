f55fe1d1a18cb169d520815e8e479a68d658cafd
Jeftifyyy <jeftifyyy@gmail.com>
2025-12-19 23:41:40 +0100

    1 philo case

--- philo/src/action.c
+++ philo/src/action.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   action.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mtran-nh <mtran-nh@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mtran-nh <mtran-nh@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/13 13:33:50 by mtran-nh          #+#    #+#             */
-/*   Updated: 2025/12/13 13:49:07 by mtran-nh         ###   ########.fr       */
+/*   Updated: 2025/12/19 23:39:55 by mtran-nh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,15 @@
 
 void        take_forks(t_philos *philos)
 {
+    if (philos->philos_count == 1)
+    {
+        pthread_mutex_lock(philos->mutex.left_fork);
+        print_action(philos, "has taken a fork");
+        while (!is_dead(philos))
+            usleep(1000);
+        pthread_mutex_unlock(philos->mutex.left_fork);
+        return;
+    }
     if (philos->id % 2 == 0)
     {
         pthread_mutex_lock(philos->mutexes.right_fork);
@@ -34,6 +43,7 @@ void        eating(t_philos *philos)
 {
     print_action(philos, "is eating");
     philos->times.last_meal = get_current_time();
+    update_last_meal(philos);
     ft_usleep(philos->times.eat);
 }
 
